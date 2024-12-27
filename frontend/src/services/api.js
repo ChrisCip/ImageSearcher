a8f330cae60a2023c59f6c99f4c1232c057338d4
CHRISTOPHER MIGUEL CIPRIAN CRUCETA <1115373@est.intec.edu.do>
2024-12-27 02:37:26 -0400

    Conexion frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://tu-backend-url.railway.app',
+    baseURL: 'imagesearcher-production.up.railway.app',
     timeout: 30000
 })
 
