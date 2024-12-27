c561f0df982b676be619cd388d117e5494c10755
CHRISTOPHER MIGUEL CIPRIAN CRUCETA <1115373@est.intec.edu.do>
2024-12-27 02:42:40 -0400

    conexion vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'imagesearcher-production.up.railway.app',
+    baseURL: 'https://imagesearcher-production.up.railway.app',
     timeout: 30000
 })
 
